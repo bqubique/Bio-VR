0219f5726fd6598ae892513d3636d3c195676b09
Blin Qipa <blinnqipa@gmail.com>
2019-07-24 16:51:46 +0300

    Improvement on QuizScene
    Added a queue to store the questions.

--- Summer Project/Assets/Scripts/QuizScript.cs
+++ Summer Project/Assets/Scripts/QuizScript.cs
@@ -5,17 +5,35 @@ using UnityEngine.UI;
 
 public class QuizScript : MonoBehaviour
 {
+    Queue<string> questionQueue = new Queue<string>();
     private Text text;
+
+    void Start()
+    {
+        fillTheQueue();
+    }
     public void OnClicked(Button Button)
     {
         string name = Button.name;
         Debug.Log("The name pressed is + " + name);
     }
 
+    private void fillTheQueue()
+    {
+        questionQueue.Enqueue("Yeps");
+        questionQueue.Enqueue("Yass");
+        questionQueue.Enqueue("Whatever");
+        questionQueue.Enqueue("Fine");
+        questionQueue.Enqueue("Afaik");
+    }
 
     public void DisplayQuestionToUser()
     {
         text = GameObject.Find("FirstTextField").GetComponent<Text>();
-        text.text = "Yeah Whatever...";
+        if(questionQueue.Count != 0)
+        {
+            text.text = questionQueue.Peek();
+            questionQueue.Dequeue();
+        }
     }
 }
