8d283f5b198ea81246051efd637019fea5359f92
Blin Qipa <blinnqipa@gmail.com>
2019-08-07 21:15:57 +0300

    Gaze Input added
    Much better in terms of user experience, we don't need an external device to control the user input anymore.

---
+++ Summer Project/Assets/Scripts/Gaze.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Gaze : MonoBehaviour
+{
+
+    public float gazeTime = 2f;
+    private float timer;
+    private bool gazedAt;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (gazedAt)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= gazeTime)
+            {
+                // execute pointerdown handler
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+                timer = 0f;
+            }
+        }
+
+    }
+
+    public void PointerEnter()
+    {
+        gazedAt = true;
+        Debug.Log("PointerEnter");
+    }
+
+    public void PointerExit()
+    {
+        gazedAt = false;
+        Debug.Log("PointerExit");
+    }
+
+    public void PointerDown()
+    {
+        Debug.Log("PointerDown");
+    }
+}
