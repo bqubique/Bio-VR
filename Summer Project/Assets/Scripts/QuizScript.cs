63235e46a90732c8c184ed8f2b421d78c715e1b4
Blin Qipa <blinnqipa@gmail.com>
2019-08-15 16:51:53 +0300

    Code cleanup, small updates
    Updates are non-functional right now.

--- Summer Project/Assets/Scripts/QuizScript.cs
+++ Summer Project/Assets/Scripts/QuizScript.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class QuizScript : MonoBehaviour
 {
-    public float timeLeft = 20.0f;
+    public float timeLeft;
     public Text timeText;
+    private int questionNo = 0;
     public Animator animator;
-    private int score = 0;
-    private bool started = false;
     private List<string> buttonList;
 
     void Start()
     {
         GameObject.Find("Welcome Screen").transform.localScale = new Vector3(0.19f, 0.19f, 0.19f);
+        GameObject.Find("Result Screen").transform.localScale = new Vector3(0, 0, 0);
         GameObject.Find("First Question").transform.localScale = new Vector3(0, 0, 0);
         GameObject.Find("Second Question").transform.localScale = new Vector3(0, 0, 0);
         GameObject.Find("Third Question").transform.localScale = new Vector3(0, 0, 0);
@@ -38,14 +38,17 @@ public class QuizScript : MonoBehaviour
         if (timeLeft > 0)
         {
             timeLeft -= Time.deltaTime;
-            timeText.text = timeLeft.ToString("0.0");
+            timeText.text = timeLeft.ToString("0");
         }
-        else if (timeLeft < 0)
+        else if (timeLeft <= 0)
         {
+            //GameEnded(0);
             Start();
         }
+
         string buttonName = EventSystem.current.currentSelectedGameObject.name;
         buttonList.Add(buttonName);
+
         foreach(string s in buttonList)
         {
             Debug.Log(s);
@@ -56,76 +59,91 @@ public class QuizScript : MonoBehaviour
     public void FirstQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("First Question","Welcome Screen"));
+        questionNo++;
     }
 
     public void SecondQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Second Question","First Question"));
+        questionNo++;
     }
 
     public void ThirdQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Third Question","Second Question"));
+        questionNo++;
     }
 
     public void FourthQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Fourth Question","Third Question"));
+        questionNo++;
     }
 
     public void FifthQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Fifth Question","Fourth Question"));
+        questionNo++;
     }
 
     public void SixthQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Sixth Question","Fifth Question"));
+        questionNo++;
     }
 
     public void SeventhQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Seventh Question","Sixth Question"));
+        questionNo++;
     }
 
     public void EighthQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Eighth Question","Seventh Question"));
+        questionNo++;
     }
 
     public void NinthQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Ninth Question", "Eighth Question"));
+        questionNo++;
     }
 
     public void TenthQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Tenth Question","Ninth Question"));
+        questionNo++;
     }
 
     public void EleventhQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Eleventh Question", "Tenth Question"));
+        questionNo++;
     }
 
     public void TwelfthQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Twelfth Question", "Eleventh Question"));
+        questionNo++;
     }
 
     public void ThirteenthQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Thirteenth Question", "Twelfth Question"));
+        questionNo++;
     }
 
     public void FourteenthQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Fourteenth Question", "Thirteenth Question"));
+        questionNo++;
     }
 
     public void FifteenthQuestion()
     {
         StartCoroutine(DisplayAfterOneSec("Fifteenth Question", "Fourteenth Question"));
+        questionNo++;
     }
     public System.Collections.IEnumerator DisplayAfterOneSec(string questionToDisplay, string questionToDisappear)
     {
@@ -134,5 +152,26 @@ public class QuizScript : MonoBehaviour
         GameObject.Find(questionToDisappear).transform.localScale = new Vector3(0, 0, 0);
         GameObject.Find(questionToDisplay).transform.localScale = new Vector3(0.19f, 0.19f, 0.19f);
         animator.Play("Fade_In");
+    }
+
+    public void GameEnded(float timeLeft)
+    {
+        GameObject.Find("Result Screen").transform.localScale = new Vector3(0.19f, 0.19f, 0.19f);
+        GameObject.Find("Welcome Screen").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("First Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Second Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Third Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Fourth Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Fifth Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Sixth Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Seventh Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Eighth Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Ninth Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Tenth Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Eleventh Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Twelfth Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Thirteenth Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Fourteenth Question").transform.localScale = new Vector3(0, 0, 0);
+        GameObject.Find("Fifteenth Question").transform.localScale = new Vector3(0, 0, 0);
     }
 }
