757fd564c3a5bb66d8b8a4b3e62013c5b60d0725
Blin Qipa <blinnqipa@gmail.com>
2019-08-19 10:25:11 +0300

    Added animation controllers
    DivisionScript provides two methods to continue and stop the animation playing.

---
+++ Summer Project/Assets/Scripts/DivisionScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DivisionScript : MonoBehaviour
+{
+    float prevSpeed;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void PauseAnim()
+    { 
+        prevSpeed = animator.speed;
+        animator.speed = 0;
+    }
+
+    public void ContinueAnim()
+    {
+        animator.speed = prevSpeed;
+    }
+}
