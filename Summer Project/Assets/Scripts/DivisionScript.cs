38b21fcfc8c0a37d382b99266b485d75f281adbd
Blin Qipa <blinnqipa@gmail.com>
2019-08-23 23:08:02 +0300

    CellDivision animation improvement
    Removed the canvas to pause/continue animation, instead we'll be using the gaze to animation to pause/continue the animation.
    General CellDivisionScene improvements.

--- Summer Project/Assets/Scripts/DivisionScript.cs
+++ Summer Project/Assets/Scripts/DivisionScript.cs
@@ -6,19 +6,33 @@ public class DivisionScript : MonoBehaviour
 {
     float prevSpeed;
     Animator animator;
+    private bool stopped = false;
 
     void Start()
     {
         animator = GetComponent<Animator>();
     }
+    
+    public void ToggleAnimation()
+    {
+        if (stopped)
+        {
+            ContinueAnim();
+        }
+        else
+            PauseAnim();
+    }
+
     public void PauseAnim()
-    { 
+    {
+        stopped = true;
         prevSpeed = animator.speed;
         animator.speed = 0;
     }
 
     public void ContinueAnim()
     {
+        stopped = false;
         animator.speed = prevSpeed;
     }
 }
